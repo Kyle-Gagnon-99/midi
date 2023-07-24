f5a32470bee178b19bc02115fd54f6eb1c3e363a
Kyle Gagnon <kmgagnon99@gmail.com>
2023-07-24 12:28:25 -0400

    Adding changes for this branch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # README
 
 This is a Rust library that is used to parse MIDI files. The library can represent the MIDI file as JSON.
+
+Just adding this to GitHub for now. Thinking of doing a different path so I need a new branch
--- tests/ingetration_tests.rs
+++ tests/ingetration_tests.rs
@@ -1,20 +1,9 @@
-use std::{sync::Once, env, fs::File, io::Write};
-
-use log::debug;
-
-static INIT: Once = Once::new();
-
-fn setup() {
-    env::set_var("RUST_LOG", "debug");
-    INIT.call_once(|| {
-        let _ = env_logger::builder().is_test(false).try_init();
-    });
-}
-
 #[test]
 #[cfg(feature = "json")]
 fn view_midi_file_data() {
-    setup();
+    use std::fs::File;
+    use std::io::Write;
+
     let midi_file = midi::Midi::new("midi_testing.mid");
     
     let json_text = midi_file.unwrap().to_json().unwrap();
